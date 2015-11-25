a6946ae4dbcc70dbad17dae24c0267ed73b6cfac
holdenhughes <holdenmh121489@gmail.com>
2015-11-25 10:57:59 -0500

    display people to html page

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,25 +1,53 @@
 (ns people-clojure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h]
+            )
   (:gen-class))
 
-(defn -main [& args]
+(defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
                       (str/split line #","))
                     people)
-    header (first people)
-    people (rest people)
-    people (map (fn [line]
-                  (interleave header line))
-                people)
-    people (map (fn [line]
-                  (apply hash-map line))
-                people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (interleave header line))
+                    people)
+        people (map (fn [line]
+                      (apply hash-map line))
+                    people)
         people (walk/keywordize-keys people)
         people (filter (fn [line]
                          (= "Brazil" (:country line)))
                        people)]
-    (spit "filtered_people.edn"
-          (pr-str people))))
+    #_(spit "filtered_people.edn"
+          (pr-str people))
+    people))
+
+(defn people-html []
+  (let [people (read-people)]
+    (map (fn [line]
+           [:br (str (:first_name line)
+                     " "
+                     (:last_name line))
+            ])
+         people))
+  )
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                      [:a {:href "http://www.theironyard.com"}
+                       "The Iron Yard"]
+                      [:br]
+                      (people-html)]])})
+
+(defn -main [& args]
+  (j/run-jetty #'handler {:port 3000 :join? false})
+  )
